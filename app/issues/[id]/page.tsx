4774ef81eb6b8c9df0770adce26b897216bf0184
Zartab Nakhwa <zartab@codewithz.com>
2024-07-25 16:46:13 +0530

    Added React Markdown

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import delay from "delay";
 import { Card, Flex, Heading, Text } from "@radix-ui/themes";
 import IssueStatusBadge from "@/app/components/IssueStatusBadge";
+import ReactMarkdown from 'react-markdown';
 
 interface Props {
   params: { id: string };
@@ -28,8 +29,10 @@ export default async function IssueDetailsPage({ params: { id } }: Props) {
         <IssueStatusBadge status={issue.status} />
         <Text>{issue.createdAt.toDateString()}</Text>
       </Flex>
-      <Card>
-        <p>{issue.description}</p>
+      <Card className="prose mt-4">
+        <ReactMarkdown>
+        {issue.description}
+        </ReactMarkdown>
       </Card>
     </div>
   );
