9b07d12f6ed3a14d4a09aa3b28acea7ee67fc738
Zartab Nakhwa <zartab@codewithz.com>
2024-07-25 16:20:29 +0530

    Show ISsues Page Styled

---
+++ app/issues/[id]/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function LoadingIssueDetails() {
+  return (
+    <div>
+      Loading...
+    </div>
+  )
+}
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,31 +1,36 @@
-import prisma from '@/prisma/client'
-import { notFound } from 'next/navigation';
-import React from 'react'
-import delay from 'delay';
+import prisma from "@/prisma/client";
+import { notFound } from "next/navigation";
+import React from "react";
+import delay from "delay";
+import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 
-interface Props{
-    params:{id:string}
+interface Props {
+  params: { id: string };
 }
 
-export default async function IssueDetailsPage({params:{id}}:Props) {
+export default async function IssueDetailsPage({ params: { id } }: Props) {
+  await delay(2000);
 
-    await delay(2000);
-    
-    const issue=await prisma.issue.findUnique({
-        where:{
-            id:parseInt(id)
-        }
-    });
+  const issue = await prisma.issue.findUnique({
+    where: {
+      id: parseInt(id),
+    },
+  });
 
-    if(!issue) {
-        notFound()
-    }
+  if (!issue) {
+    notFound();
+  }
   return (
     <div>
-      <p>{issue.title}</p>
-      <p>{issue.description}</p>
-      <p>{issue.status}</p>
-      <p>{issue.createdAt.toDateString()}</p>
+      <Heading>{issue.title}</Heading>
+      <Flex gap="3" my="2">
+        <IssueStatusBadge status={issue.status} />
+        <Text>{issue.createdAt.toDateString()}</Text>
+      </Flex>
+      <Card>
+        <p>{issue.description}</p>
+      </Card>
     </div>
-  )
+  );
 }
